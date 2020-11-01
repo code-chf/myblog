9bce31cff70bcf0bce9822fb59bae30f05a8c510
CHENG-H-F <chenghf122112@outlook.com>
2020-11-01 14:14:42 +0800

    rebuilding site 2020年11月 1日 星期日 14时14分42秒 CST

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,9 @@
-#本脚本实现了同时部署到gitee码云和github
+#该脚本实现myblog/content下的文章更新、config.toml配置文件更新和同时部署到gitee码云和github
 
-
-#自动部署到gitee码云
-#!/bin/sh
-echo "\033[0;32mDeploying updates to Gitee...\033[0m"
-
-# 用hugo建立一个静态站点
-hugo --baseURL="https://codechf.gitee.io"
-
-# Go To Public folder
-cd public
-# Add changes to git.
-# git init
-git add .
+#git博客站点主要文件
+git add config.toml
+git add content/*
+git add deploy.sh
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -20,52 +11,57 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
 #删除url并重新添加url
 git remote rm origin
-git remote add origin https://gitee.com/codechf/codechf.git
-
-# 合并gitee上的文件
-# git pull origin master --allow-unrelated-histories
-
-# Push source and build repos.
+git remote add origin https://github.com/code-chf/myblog.git
 git push -u origin master
 
-# Come Back up to the Project Root
-cd ..
 
 
-#同步部署到github上面
-#!/bin/sh
-echo "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-#hugo --baseURL="https://code-chf.github.io"
-hugo
-
-#自定义创建public_github发布目录
-hugo --destination="public_github"
-
+#自动部署到gitee码云
+echo "\033[0;32mDeploying updates to Gitee...\033[0m"
+# 用hugo建立一个静态站点
+hugo --baseURL="https://codechf.gitee.io"
 # Go To Public folder
-cd public_github
-
+cd public
+# Add changes to git.
 # git init
 git add .
-
+# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
+#删除url并重新添加url
+git remote rm origin
+git remote add origin https://gitee.com/codechf/codechf.git
+# Push source and build repos.
+git push -u origin master
+# Come Back up to the Project Root
+cd ..
 
+
+
+#同步部署到github上面
+echo "\033[0;32mDeploying updates to GitHub...\033[0m"
+hugo
+#自定义创建public_github发布目录
+hugo --destination="public_github"
+# Go To Public folder
+cd public_github
+# git init
+git add .
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
 #删除url并重新添加url
 git remote rm origin
 git remote add origin https://github.com/code-chf/code-chf.github.io.git
-
-# 合并github上的文件
-# git pull origin master --allow-unrelated-histories
-
 # Push source and build repos.
 git push -u origin master
-
 # Come Back up to the Project Root
 cd ..
