b5000d02a019f1b7044389cc25273e2888be96e1
CHENG-H-F <chenghf122112@outlook.com>
2021-02-02 11:34:08 +0800

    rebuilding site 2021年 2月 2日 星期二 11时34分08秒 CST

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #该脚本实现myblog/content下的文章更新、config.toml配置文件更新和同时部署到gitee码云和github
 
+echo "\033[0;32m开始同步hugo博客站点！!\033[0m"
 #git博客站点主要文件
 git add config.toml
 git add content/*
@@ -16,11 +17,11 @@ git commit -m "$msg"
 git remote rm origin
 git remote add origin https://github.com/code-chf/myblog.git
 git push -u origin master
-
+echo "\033[0;32mHugo站点同步成功！!\033[0m"
 
 
 #自动部署到gitee码云
-echo "\033[0;32mDeploying updates to Gitee...\033[0m"
+echo "\033[0;32m开始部署到Gitee...\033[0m"
 # 用hugo建立一个静态站点
 hugo --baseURL="https://codechf.gitee.io"
 # Go To Public folder
@@ -41,11 +42,11 @@ git remote add origin https://gitee.com/codechf/codechf.git
 git push -u origin master
 # Come Back up to the Project Root
 cd ..
-
+echo "\033[0;32mGitee部署成功！\033[0m"
 
 
 #同步部署到github上面
-echo "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo "\033[0;32m开始部署到GitHub...\033[0m"
 #自定义创建public_github发布目录
 hugo --destination="public_github"
 # Go To Public folder
@@ -65,10 +66,10 @@ git remote add origin https://github.com/code-chf/code-chf.github.io.git
 git push -u origin master
 # Come Back up to the Project Root
 cd ..
-
+echo "\033[0;32mGithub部署成功！\033[0m"
 
 #部署到个人本地服务器
-echo "\033[0;32mDeploying updates to Server...\033[0m"
+echo "\033[0;32m开始部署到远程服务器...\033[0m"
 #自定义创建public_server发布目录
 hugo --destination="public_server" --baseURL="/"
 # Go To Public folder
@@ -95,4 +96,5 @@ cd /root/myblog/public_server
 git pull /home/gitrepo/public_server.git
 exit
 eeooff
-echo done!
+echo "\033[0;32m远程服务器部署成功！\033[0m"
+echo "\033[0;32m脚本运行完毕！\033[0m"
