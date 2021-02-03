845e827fd9a33a11c447d5720ca6efff9bf6e26d
CHENG-H-F <chenghf122112@outlook.com>
2021-02-03 20:54:52 +0800

    rebuilding site 2021年 2月 3日 星期三 20时54分52秒 CST

--- deploy.sh
+++ deploy.sh
@@ -68,7 +68,7 @@ git push -u origin master
 cd ..
 echo "\033[0;32mGithub部署成功！\033[0m"
 
-#部署到个人本地服务器
+#部署到codechf个人本地服务器
 echo "\033[0;32m开始部署到远程服务器...\033[0m"
 #自定义创建public_server发布目录
 hugo --destination="public_server" --baseURL="/"
